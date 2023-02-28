4789e494b98ff041c3dc60169175970bc9458c77
carloseduneves <carlosneves.edu@gmail.com>
2023-02-27 23:33:47 -0300

    Louvor -Cosc 1.0.2

--- src/components/Song.js
+++ src/components/Song.js
@@ -1,12 +1,21 @@
 import React from 'react'
 
+import './Song.css';
+
 function Song({name, letter, video}) {
   return (
     <div className="box">
-        <h2>{name}</h2>
-        <div>
-            <a href={letter} className="links">letra/cifra</a> <a href={video} className="links">ouvir</a>
+        <h2 className='title'>{name}</h2>
+        <div className='options'>
+          <div className='links'>
+              <a href={letter} className="links">letra</a>
+          </div>
+          <div className='links'>
+            <a href={video} className="links" target="_blank"  rel="noreferrer">ouvir</a>
+            </div>
+           
         </div>
+       
     </div>
   )
 }
